041ce19f187b865a82925d1319ec895e1469ed5e
Mariana Explica <marianaexplica@example.com>
2026-06-12 18:10:26 -0300

    Revert "Fix managed page root top padding"
    This reverts commit 63d115b067e6f6e269e4fb0d0eedbe5eadce877e.

--- build-info.ts
+++ build-info.ts
@@ -1,6 +1,6 @@
 export const BUILD_VERSION_NUMBER = "1.0.0"
-export const BUILD_DEPLOY_NUMBER = "73"
-export const BUILD_COMMIT = "padtop0-20260612"
+export const BUILD_DEPLOY_NUMBER = "72"
+export const BUILD_COMMIT = "gemini3510720612"
 
 export const BUILD_LABEL = `${BUILD_VERSION_NUMBER}-${BUILD_DEPLOY_NUMBER}-${BUILD_COMMIT}`
 
