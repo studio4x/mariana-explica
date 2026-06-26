1a1521cf3e36e35b371fec311ce9ba561d297d01
Mariana Explica <marianaexplica@example.com>
2026-06-26 11:39:06 -0300

    Update admin build label

--- build-info.ts
+++ build-info.ts
@@ -1,6 +1,6 @@
 export const BUILD_VERSION_NUMBER = "1.0.0"
-export const BUILD_DEPLOY_NUMBER = "144"
-export const BUILD_COMMIT = "legacy-public-editor-reactivation"
+export const BUILD_DEPLOY_NUMBER = "145"
+export const BUILD_COMMIT = "site-typography-sitewide"
 
 export const BUILD_LABEL = `${BUILD_VERSION_NUMBER}-${BUILD_DEPLOY_NUMBER}-${BUILD_COMMIT}`
 
