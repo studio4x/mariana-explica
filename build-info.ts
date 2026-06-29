3faa1e396fdef35d9751ebc1cb43590c6956f578
Mariana Explica <marianaexplica@example.com>
2026-06-29 10:39:45 -0300

    Set stable build label for cache release

--- build-info.ts
+++ build-info.ts
@@ -1,6 +1,6 @@
 export const BUILD_VERSION_NUMBER = "1.0.0"
-export const BUILD_DEPLOY_NUMBER = "153"
-export const BUILD_COMMIT = "b137617"
+export const BUILD_DEPLOY_NUMBER = "154"
+export const BUILD_COMMIT = "cache-controls"
 
 export const BUILD_LABEL = `${BUILD_VERSION_NUMBER}-${BUILD_DEPLOY_NUMBER}-${BUILD_COMMIT}`
 
