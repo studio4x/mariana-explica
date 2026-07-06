be96aaae3d6f2bfe0631fbbb055f726403d69cff
Mariana Explica <marianaexplica@example.com>
2026-07-06 10:31:56 -0300

    Bump build version for courses progress refresh

--- build-info.ts
+++ build-info.ts
@@ -1,6 +1,6 @@
 export const BUILD_VERSION_NUMBER = "1.0.0"
-export const BUILD_DEPLOY_NUMBER = "187"
-export const BUILD_COMMIT = "student-builder-menu-active-state"
+export const BUILD_DEPLOY_NUMBER = "188"
+export const BUILD_COMMIT = "student-courses-progress-cache-refresh"
 
 export const BUILD_LABEL = `${BUILD_VERSION_NUMBER}-${BUILD_DEPLOY_NUMBER}-${BUILD_COMMIT}`
 
