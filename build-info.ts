bb439561de65c63c8dedbe476917574e6fbb6d9c
Mariana Explica <marianaexplica@example.com>
2026-07-13 18:44:15 -0300

    chore(build): atualizar etiqueta do leitor pdf

--- build-info.ts
+++ build-info.ts
@@ -1,6 +1,6 @@
 export const BUILD_VERSION_NUMBER = "1.0.0"
-export const BUILD_DEPLOY_NUMBER = "236"
-export const BUILD_COMMIT = "e6a9e57"
+export const BUILD_DEPLOY_NUMBER = "237"
+export const BUILD_COMMIT = "cb89db6"
 
 export const BUILD_LABEL = `${BUILD_VERSION_NUMBER}-${BUILD_DEPLOY_NUMBER}-${BUILD_COMMIT}`
 
