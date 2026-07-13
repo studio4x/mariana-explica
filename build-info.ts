622615bd1e711983e6f808380ec7831f35f2c5ad
Mariana Explica <marianaexplica@example.com>
2026-07-13 18:51:10 -0300

    chore(build): atualizar etiqueta da persistencia

--- build-info.ts
+++ build-info.ts
@@ -1,6 +1,6 @@
 export const BUILD_VERSION_NUMBER = "1.0.0"
-export const BUILD_DEPLOY_NUMBER = "237"
-export const BUILD_COMMIT = "cb89db6"
+export const BUILD_DEPLOY_NUMBER = "238"
+export const BUILD_COMMIT = "7118664"
 
 export const BUILD_LABEL = `${BUILD_VERSION_NUMBER}-${BUILD_DEPLOY_NUMBER}-${BUILD_COMMIT}`
 
