c57bc66ffe9b380cafe9c081d19296765c6358a8
Mariana Explica <marianaexplica@example.com>
2026-07-15 09:07:47 -0300

    chore(build): atualizar etiqueta do Stripe Tax

--- build-info.ts
+++ build-info.ts
@@ -1,6 +1,6 @@
 export const BUILD_VERSION_NUMBER = "1.0.0"
-export const BUILD_DEPLOY_NUMBER = "247"
-export const BUILD_COMMIT = "9227f13"
+export const BUILD_DEPLOY_NUMBER = "248"
+export const BUILD_COMMIT = "582b237"
 
 export const BUILD_LABEL = `${BUILD_VERSION_NUMBER}-${BUILD_DEPLOY_NUMBER}-${BUILD_COMMIT}`
 
