8ce1b8d36fb5578e3a3292ec84ef23ed3cf29119
Mariana Explica <marianaexplica@example.com>
2026-07-16 16:03:26 -0300

    chore(build): atualizar versao do suporte

--- build-info.ts
+++ build-info.ts
@@ -1,6 +1,6 @@
 export const BUILD_VERSION_NUMBER = "1.0.0"
-export const BUILD_DEPLOY_NUMBER = "251"
-export const BUILD_COMMIT = "b660f08"
+export const BUILD_DEPLOY_NUMBER = "252"
+export const BUILD_COMMIT = "106e876"
 
 export const BUILD_LABEL = `${BUILD_VERSION_NUMBER}-${BUILD_DEPLOY_NUMBER}-${BUILD_COMMIT}`
 
